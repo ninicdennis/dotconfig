35dfe31e8365d44e0a9ec1d69e272944174d6aaf
ninicdennis <pupperbot@protonmail.com>
2023-02-19 12:23:13 -0700

    Updated lvim config with comments

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -1,11 +1,13 @@
 -- Additional Plugins
 lvim.plugins = {
+  -- Auto Tagging
   {
     "windwp/nvim-ts-autotag",
     config = function()
       require("nvim-ts-autotag").setup()
     end,
   },
+  -- Rust Crate command
   {
     "saecki/crates.nvim",
     tag = "v0.3.0",
@@ -22,14 +24,19 @@ lvim.plugins = {
       }
     end,
   },
+  -- LSP progress
   {
     "j-hui/fidget.nvim",
     config = function()
       require("fidget").setup()
     end,
   },
+  -- Rust tools
   "simrat39/rust-tools.nvim",
+
+  -- Inlay hints for LSP
   "lvimuser/lsp-inlayhints.nvim",
+  -- Color coded comments
   {
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
@@ -37,10 +44,39 @@ lvim.plugins = {
       require("todo-comments").setup()
     end
   },
+  -- Colorizer for nvim
   { "norcalli/nvim-colorizer.lua",
     config = function()
       require("colorizer").setup()
     end
   },
+  -- Theme
   { "catppuccin/nvim", as = 'catppuccin' },
+  -- Markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    ft = "markdown",
+    config = function()
+      vim.g.mkdp_auto_start = 1
+    end,
+  },
+  -- Rainbow comments
+  {
+    "mrjones2014/nvim-ts-rainbow",
+  },
+  -- Open back up to last place
+  {
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = {
+          "gitcommit", "gitrebase", "svn", "hgcommit",
+        },
+        lastplace_open_folds = true,
+      })
+    end,
+  },
 }
